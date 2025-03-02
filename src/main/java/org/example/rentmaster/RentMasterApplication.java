0cd480b5ec4be205568a9075b070f675623f2d1d
mustafayacoub2024 <mustafa.yacoub2013@mail.ru>
2025-03-02 11:50:57 +0300

    first commit

---
+++ src/main/java/org/example/rentmaster/RentMasterApplication.java
@@ -0,0 +1,13 @@
+package org.example.rentmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/rentmaster/RentMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rentmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
