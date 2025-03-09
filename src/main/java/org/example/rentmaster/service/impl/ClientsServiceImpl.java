ed8f859d1d0409bba28a4f9c5afd94dac702aa32
mustafayacoub2024 <mustafa.yacoub2013@mail.ru>
2025-03-09 16:42:04 +0300

    1.Добавил контроллер, сервис, хендлер, конфигурации для OpenApi

---
+++ src/main/java/org/example/rentmaster/service/impl/ClientsServiceImpl.java
@@ -0,0 +1,12 @@
+package org.example.rentmaster.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.rentmaster.service.ClientsService;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ClientsServiceImpl implements ClientsService {
+}
---
+++ src/main/java/org/example/rentmaster/service/impl/EmployeesServiceImpl.java
@@ -0,0 +1,12 @@
+package org.example.rentmaster.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.rentmaster.service.EmployeesService;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EmployeesServiceImpl implements EmployeesService {
+}
