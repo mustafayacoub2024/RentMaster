af353ea8bcece79c66da4d620073ae7e8d04b1f5
Mustafa <mustafa.yacoub2013@mail.ru>
2025-03-12 20:33:12 +0300

    Исправил помник и docker-compose

--- src/main/java/org/example/rentmaster/exception/CommonBackendException.java
+++ src/main/java/org/example/rentmaster/exception/CommonBackendException.java
@@ -1,12 +1,12 @@
-package org.example.rentmaster.exception;
-
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-
-@Getter
-@RequiredArgsConstructor
-public class CommonBackendException extends RuntimeException {
-    private final String message;
-    private final HttpStatus status;
-}
+//package org.example.rentmaster.exception;
+//
+//import lombok.Getter;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//
+//@Getter
+//@RequiredArgsConstructor
+//public class CommonBackendException extends RuntimeException {
+//    private final String message;
+//    private final HttpStatus status;
+//}
--- src/main/java/org/example/rentmaster/exception/ErrorMessage.java
+++ src/main/java/org/example/rentmaster/exception/ErrorMessage.java
@@ -1,12 +1,12 @@
-package org.example.rentmaster.exception;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-public class ErrorMessage {
-    private String message;
-}
+//package org.example.rentmaster.exception;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//public class ErrorMessage {
+//    private String message;
+//}
