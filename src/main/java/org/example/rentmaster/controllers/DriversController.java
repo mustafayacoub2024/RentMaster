75f6b1184d15245e003b12d07594fae9248e0b2b
Mustafa <mustafa.yacoub2013@mail.ru>
2025-03-16 17:14:12 +0300

    Исправление контролера Drivers

--- src/main/java/org/example/rentmaster/controllers/DriversController.java
+++ src/main/java/org/example/rentmaster/controllers/DriversController.java
@@ -1,4 +1,11 @@
 package org.example.rentmaster.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/drivers")
+@RequiredArgsConstructor
 public class DriversController {
 }
